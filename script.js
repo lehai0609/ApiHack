fa92868bfd1ba50b96fa7a032251accedebfcbe2
lehai0609 <lehai0609@gmail.com>
2014-01-05 02:47:14 +0700

    Finalised to commit
    FInal

--- script.js
+++ script.js
@@ -5,14 +5,16 @@
  */
   $(document).ready(function() {
       initialize();
-      $("form").submit(route);
+      $("form").submit(calcRoute);
+      $("#clear").click(clearMarker);
   });
     var map = null;
+    var gresults = [];
     var directions = null;
     var routeBoxer = null;
     var bounds;
-    var distance = 50; //km
-    
+    var distance = null; //km
+    var directionService = new google.maps.DirectionsService();
     var initialize = function () {
       // Default the map view to the continental U.S.
       var mapOptions = {
@@ -21,40 +23,45 @@
         zoom: 4
       };
       map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-      
-      directionService = new google.maps.DirectionsService();
       directionsRenderer = new google.maps.DirectionsRenderer();
       directionsRenderer.setMap(map);
     };
     
-    var route = function () {
+    var calcRoute = function () {
+     
+      routeBoxer = new RouteBoxer();
       var request = {
         origin: document.getElementById("from").value,
         destination: document.getElementById("to").value,
         travelMode: google.maps.TravelMode.DRIVING
-      }
+      };
       // Make the directions request
-      directionService.route(request, function(result, status) {
+      directionService.route(request, function(response, status) {
         if (status == google.maps.DirectionsStatus.OK) {
-          directionsRenderer.setDirections(result);
+          directionsRenderer.setDirections(response);
           // Box around the overview path of the first route
-          var path = result.routes[0].overview_path;
+          distance = document.getElementById("range").value;
+          var path = response.routes[0].overview_path;
           var boxes = routeBoxer.box(path, distance);
           for (var i = 0; i < boxes.length; i++) {
               bounds = boxes[i];
           //Places service over boxes[i]
-              placeService(bounds);  
+              placeService(bounds);
             }
         } else {
           alert("Directions query failed: " + status);
         }
       });
-    }
+     return false;
+    };
     
     var placeService = function(bounds) {
+        
+        var selected_Index = document.userInput.userSelect.selectedIndex;
+        var typeOfPlace = document.userInput.userSelect.options[selected_Index].value;
         var request = {
             bounds: bounds,
-            types: ['store']
+            types: [typeOfPlace]
         };
         infowindow = new google.maps.InfoWindow();
         var service = new google.maps.places.PlacesService(map);
@@ -68,4 +75,26 @@
             }
         }     
     }
- 
+    
+    var createMarker = function(results) {
+        /*var img = {
+            url: results.icon,
+            size: new google.maps.Size(20,32),
+            origin: new google.maps.Point(0,0),
+            anchor: new google.maps.Point(0, 32)
+        };*/
+        var marker = new google.maps.Marker({
+            position: results.geometry.location,
+            map: map,
+            //icon: img
+            title:results.name + ": "+ results.vicinity
+        });
+        gresults.push(marker);
+    };
+    
+    var clearMarker = function() {
+        for (var i=0; i<gresults.length; i++){
+            gresults[i].setMap(null); 
+        };
+    };
+    
